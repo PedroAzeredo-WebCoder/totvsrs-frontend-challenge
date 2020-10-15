3c1f24b70d7143386088a878e0b1beec249e30c5
Pedro Azeredo <pedro.azeredo93@gmail.com>
2020-10-15 07:22:52 -0300

    Adiciomando box de confirmação de exclusao

--- assets/js/modalButtons.js
+++ assets/js/modalButtons.js
@@ -1,3 +1,35 @@
 function deleteFunction(){
-    confirm("Os itens serão excluidos!");
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success mx-3',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Você tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Excluir',
+        cancelButtonText: 'Cancelar',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Excluido com sucesso!',
+            'Seu arquivo foi excluído.',
+            'Success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado!',
+            'Error'
+          )
+        }
+      })
 }
